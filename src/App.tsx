13bb44fc4558453f5476f80d49f671753bfc8ece
leesangwon <tkddnjs5613@naver.com>
2022-06-17 05:39:45 +0900

    fix: husky & lint-staged 작동됨
    husky & lint-staged 작동이 안되서 npx mrm@2 lint-staged 모듈 설치 후 작동됨

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  const a = 6;
-  const b = 5;
-  const c = 4;
-  const d = 5;
-  return <div className="App">푸드솔로몬</div>;
+  return <div className='App'>푸드솔로몬</div>;
 }
 
 export default App;
